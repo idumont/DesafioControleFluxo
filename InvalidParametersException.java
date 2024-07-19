5abbf886de9ca545e4f4f7d874d99f46798384d5
IEDA DUARTE <iedadumont80@gmail.com>
2024-07-19 12:55:54 -0300

    Add files via upload

---
+++ InvalidParametersException.java
@@ -0,0 +1,8 @@
+public class InvalidParametersException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public InvalidParametersException(String message) {
+		super(message);
+	}
+}
